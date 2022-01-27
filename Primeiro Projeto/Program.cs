0c7a96d92e6b080f88974f7dca3b123c172b4edd
Genilson Gabriel Ferreira <genilsongs@gmail.com>
2022-01-27 00:26:53 -0300

    Add project files.

---
+++ Primeiro Projeto/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Primeiro_Projeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   var a = "Hello ";
+            var b = "World";
+            Console.WriteLine(a + b);
+        }
+    }
+}
